8b9872950a4d8a649e313b67f7094b3d6fc82bce
synx <synnus@gmail.com>
2019-06-27 11:37:03 +0200

    Add files via upload

---
+++ test.bat
@@ -0,0 +1,5 @@
+vs_community.exe --layout c:\vs2017offline --lang en-US --add Microsoft.VisualStudio.Workload.NativeDesktop microsoft.visualstudio.component.vc.tools.x86.x64 microsoft.visualstudio.component.windows10sdk.17763
+cd c:\vs2017offline
+vs_community.exe
+pause
+ 
